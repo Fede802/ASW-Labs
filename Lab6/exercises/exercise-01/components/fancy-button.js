abb25fd45cb6476124a833a1be3aebf54d38763a
Federico Bravetti <federicobravetti802@gmail.com>
2024-11-30 14:29:56 +0100

    add Lab7

--- Lab6/exercises/exercise-01/components/fancy-button.js
+++ Lab6/exercises/exercise-01/components/fancy-button.js
@@ -1,1 +1,21 @@
-const FancyButton = Vue.defineComponent();
+const FancyButton = Vue.defineComponent({
+    template:`<button :class="last_clicked ? 'bg-green-500' : 'bg-blue-500'" class = "my-14 px-4 py-2 text-white rounded"  v-on:click="clicked"><slot></slot>
+            [x{{ multiplier }}] ({{ clicks }} click)
+        </button>`,
+    data(){
+        return {
+            clicks: 0
+        }
+    },   
+    props: {
+        id: String,
+        multiplier: Number,
+        last_clicked: Boolean
+    },
+    methods: {
+        clicked() {
+            this.clicks += 1;
+            this.$emit('clicked-event', this.id, this.multiplier);
+        }
+    }
+});
