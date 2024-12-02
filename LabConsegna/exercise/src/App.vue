885a8fe1010ca4938a8cf6c6056b415c04e8482b
Federico Bravetti <federicobravetti802@gmail.com>
2024-12-02 23:57:01 +0100

    refactor

--- LabConsegna/exercise/src/App.vue
+++ LabConsegna/exercise/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import axios from 'axios';
-import AdvancedMinimalOptionApiTable from './minimal-options-api/AdvancedTable.vue';
-import AdvancedAccessibleOptionsApiTable from './accessible-options-api/AdvancedTable.vue';
+import MinimalOptionApiTable from './minimal-options-api/Table.vue';
+import AccessibleOptionsApiTable from './accessible-options-api/Table.vue';
 //Todo
 import MinimalCompositionApiTable from './minimal-composition-api/Table.vue';
 
@@ -9,8 +9,8 @@ import AccessibleCompositionApiTable from './accessible-composition-api/Table.vu
 </script>
 
 <template>
-    <AdvancedMinimalOptionApiTable />
-    <AdvancedAccessibleOptionsApiTable />
+    <MinimalOptionApiTable />
+    <AccessibleOptionsApiTable />
 
     <MinimalCompositionApiTable />
     <AccessibleCompositionApiTable />
--- LabConsegna/exercise/src/accessible-options-api/Table.vue
+++ LabConsegna/exercise/src/accessible-options-api/Table.vue
@@ -1,21 +1,41 @@
 <script>
-import { loadData } from "../utils/dbUtils";
-import TableHeader from "../commons-options-api/TableHeader.vue";
-import TableData from "../commons-options-api/TableData.vue";
-import PlainText from "../commons-options-api/PlainText.vue";
 import { h } from 'vue';
+import Image from "@/commons-options-api/Image.vue";
+import PlainText from "@/commons-options-api/PlainText.vue";
+import List from "@/commons-options-api/List.vue";
+import Link from "@/commons-options-api/Link.vue";
+import TableHeader from "@/commons-options-api/TableHeader.vue";
+import TableData from "@/commons-options-api/TableData.vue";
+import { loadData } from "../utils/dbUtils";
 
 export default {
-    components: { TableHeader, TableData, PlainText },
+    components: { Image, PlainText, List, Link, TableHeader, TableData },
     data() {
         return {
             data: [],
+            dbUrl: "http://localhost:3000/recipes_with_alt.json",
+            dbParser: function (db) { return db.slice(0,2) },
+            tableBuildingData: {
+                "Name": function (recipe) {
+                    return h(PlainText, { text: recipe.name })
+                },
+                "Description": function (recipe) {
+                    return h(PlainText, { text: recipe.description })
+                },
+                "Image": function (recipe) {
+                    return h(Image, { img: recipe.image, alt: recipe.alt })
+                },
+                "Nutrients": function (recipe) {
+                    return h(List, { list: Object.entries(recipe.nutrients).map(k => k[0]+": "+k[1]) })
+                },
+                "Author": function (recipe) {
+                    return h(PlainText, { text: recipe.author })
+                },
+                "Website": function (recipe) {
+                    return h(Link, { link: recipe.url, text: "Visit Website", style: {'white-space': 'nowrap'} })
+                },
+            }
         }
-    },
-    props: {
-        dbUrl:String,
-        dbParser: Function,
-        tableBuildingData: Object,
     },
     mounted() {
         loadData(this.dbUrl).then(response => {
@@ -26,9 +46,10 @@ export default {
 </script>
 
 <template>
+    <h1>Accessible Options API Table</h1>
     <div class="table responsive">
         <table class="table" v-if="tableBuildingData != null">
-            <thead class="thead-dark">
+            <thead class="table-dark">
                 <TableHeader v-bind:headers="Object.keys(tableBuildingData)" scopeInfo="col"></TableHeader>
             </thead>
             <tbody>
@@ -37,3 +58,10 @@ export default {
         </table>
     </div>
 </template>
+
+<style scoped>
+    ::v-deep th, ::v-deep td {
+        border: 2px solid #000000;
+        vertical-align: middle;
+    }
+</style>
