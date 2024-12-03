16a1d172f45cfbc5c68b7bdfd6bc70ee23b9e93b
Federico Bravetti <federicobravetti802@gmail.com>
2024-12-03 01:45:04 +0100

    refactor

--- LabConsegna/exercise/src/App.vue
+++ LabConsegna/exercise/src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
-import axios from 'axios';
 import MinimalOptionApiTable from './minimal-options-api/Table.vue';
 import AccessibleOptionsApiTable from './accessible-options-api/Table.vue';
 //Todo
 import MinimalCompositionApiTable from './minimal-composition-api/Table.vue';
 
 import AccessibleCompositionApiTable from './accessible-composition-api/Table.vue';
+
 </script>
 
 <template>
     <MinimalOptionApiTable />
     <AccessibleOptionsApiTable />
 
-    <MinimalCompositionApiTable />
-    <AccessibleCompositionApiTable />
+    <!-- <MinimalCompositionApiTable /> -->
+    <!-- <AccessibleCompositionApiTable /> -->
 </template>
-
-
