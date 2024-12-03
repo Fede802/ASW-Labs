d94f9a7afe80b6aba6394482b5aa4a7e04387d7a
Federico Bravetti <federicobravetti802@gmail.com>
2024-12-03 01:04:21 +0100

    working state

--- LabConsegna/exercise/src/App.vue
+++ LabConsegna/exercise/src/App.vue
@@ -6,11 +6,9 @@ import AccessibleOptionsApiTable from './accessible-options-api/Table.vue';
 import MinimalCompositionApiTable from './minimal-composition-api/Table.vue';
 
 import AccessibleCompositionApiTable from './accessible-composition-api/Table.vue';
-import Test from './Test.vue';
 </script>
 
 <template>
-    <Test />
     <MinimalOptionApiTable />
     <AccessibleOptionsApiTable />
 
